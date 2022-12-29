26d55fb5eabebc237dae59352532dde39f325992
Akbar Haryadi <akbarharyadi777@gmail.com>
2022-12-29 20:36:21 +0700

    server port

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from './app';
 
 const server: Server = createServer(app);
 const hostname = '0.0.0.0';
-const port = 4000;
+const port = 5000;
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
